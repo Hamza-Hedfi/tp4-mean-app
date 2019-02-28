4d1c7d1765a91a207e65dafbc321dc50d0f978c8
Hamza Hedfi <hedfi.hamza@hotmail.fr>
2019-02-28 19:10:38 +0100

    Fix typo

--- backend/models/contact.js
+++ backend/models/contact.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 
 const contactSchema = mongoose.Schema({
   name: { type: String, required: true },
-  email: { tyep: String, required: true },
-  phone: { tyep: String, required: true }
+  email: { type: String, required: true },
+  phone: { type: String, required: true }
 });
 
 module.exports = mongoose.model('Contact', contactSchema);
